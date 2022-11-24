ef8458e11c436cc35449256e41e13d6544ab92f8
Safwan <safwanridwan321@gmail.com>
2022-11-24 16:41:04 +0600

    added category

--- src/Pages/Home/Categories/Category/Category.js
+++ src/Pages/Home/Categories/Category/Category.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import CategoryCard from './CategoryCard';
 
 const Category = () => {
     const phones = useLoaderData();
     console.log(phones)
     return (
-        <div>
-            <h2>this is category</h2>
+        <div className='w-11/12 mx-auto my-10'>
+            <h2 className='text-2xl font-semibold'>results for: {phones[0].category}</h2>
+            <div className='my-10'>
+                {
+                    phones.map((phone, i) => <CategoryCard
+                        key={i}
+                        phone={phone}
+                    ></CategoryCard>)
+                }
+            </div>
         </div>
     );
 };
